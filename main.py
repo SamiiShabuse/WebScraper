c6515219273762d02e85a453f2f7bf770ecb12c2
Samii Shabuse <sshabuse@gmail.com>
2025-04-23 19:47:14 -0400

    feat(scraper): add get_school_links to get schools near given query

--- main.py
+++ main.py
@@ -7,7 +7,36 @@ from urllib.parse import urljoin
 from dotenv import load_dotenv
 import os
 
+# CONFIGURATION
 load_dotenv()
 SERPAPI_KEY = os.getenv("SERPAPI_KEY")
-print(SERPAPI_KEY)
+SEARCH_QUERY = "high schools near Drexel University"
+EXCEL_FILENAMES = "drexel_highschools.xlsx"
 
+# STEP 1: GET SCHOOL LINKS FROM GOOGLE
+def get_school_links(query, api_key):
+    print("[*] Searching for school websites...")
+    params = {
+            "engine": "google",
+            "q": query,
+            "api_key": api_key,
+    }
+    response = requests.get("https://serpapi.com/search", params=params)
+    results = response.json()
+
+    links = []
+    for result in results.get("organic_results", []):
+        link = result.get("link")
+        if link:
+            links.append(link)
+    return links
+
+
+# STEP 2: FIND STAFF/DIRECTORY PAGE
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    
